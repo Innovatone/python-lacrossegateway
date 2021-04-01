994516cab9cf86d208ff978cd66d675517612ecb
Oliver Novakovic <innovatone@gmail.com>
2021-04-01 15:26:16 +0200

    changed sensorname

--- pylacrossegateway/__init__.py
+++ pylacrossegateway/__init__.py
@@ -15,4 +15,4 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
 # USA
 
-from .lacrossegateway import LaCrosseGateway, LaCrosseGatewaySensor
+from .lacrossegateway import LaCrosseGateway, LaCrosseSensor
